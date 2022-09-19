d75172c4c12dbfe2534d1d72b53f8ef8e4c1569d
Kai Franke <cosinushh@gmail.com>
2022-09-19 21:46:25 +0200

    change minor css styles

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -27,7 +27,7 @@ function Card() {
           <svg
             className="bookmark__icon"
             xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 25 25"
+            viewBox="0 0 24 23"
           >
             <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z" />
           </svg>
